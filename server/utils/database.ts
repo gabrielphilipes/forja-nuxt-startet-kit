8560334d52a5112d9e934ee37daa179f40dce632
Gabriel Philipe S <48845302+gabrielphilipes@users.noreply.github.com>
2025-06-27 09:48:25 -0300

    feat(drizzle): install and configure connection in database; endpoint to rescue infors

---
+++ server/utils/database.ts
@@ -0,0 +1,30 @@
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool, type QueryResult } from 'pg'
+
+export const dbCredentials = {
+  host: process.env.POSTGRES_HOST!,
+  database: process.env.POSTGRES_DB!,
+  port: parseInt(process.env.POSTGRES_PORT!),
+  user: process.env.POSTGRES_USER!,
+  password: process.env.POSTGRES_PASSWORD!,
+  ssl: false
+}
+
+const poolConnection = new Pool({ ...dbCredentials })
+
+export const useDB = (): ReturnType<typeof drizzle> => {
+  return drizzle({ client: poolConnection })
+}
+
+export const useQueryDB = async (query: string, params?: unknown[]): Promise<QueryResult> => {
+  try {
+    const client = useDB().$client
+    return await client.query(query, params)
+  } catch (error) {
+    console.error(error)
+    throw createError({
+      statusCode: 500,
+      message: 'Erro na conexão com o banco de dados'
+    })
+  }
+}
