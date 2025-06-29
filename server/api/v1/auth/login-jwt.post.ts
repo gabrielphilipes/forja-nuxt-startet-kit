de2a2aa401f60be2297ad54d154af4e4cadb8d70
Gabriel Philipe S <48845302+gabrielphilipes@users.noreply.github.com>
2025-06-29 20:21:23 -0300

    feat(auth): implement JWT login with validation token generation

---
+++ server/api/v1/auth/login-jwt.post.ts
@@ -0,0 +1,40 @@
+import { LoginUserSchema } from '#server/utils/validations/auth'
+import type { User } from '#server/database/schemas/users'
+import { generateJWTToken } from '#server/utils/auth'
+import user from '#server/models/user'
+
+export default defineEventHandler(async (event) => {
+  const { success, data, error } = await readValidatedBody(event, (body) =>
+    LoginUserSchema.safeParse(body)
+  )
+
+  if (!success) {
+    throw createErrorValidation('Ajuste os dados enviados e tente novamente', error)
+  }
+
+  let loginUser: User | null = null
+
+  try {
+    loginUser = await user.loginWithPassword(data.email, data.password)
+  } catch (error) {
+    console.error(error)
+    throw createError({ statusCode: 401, message: 'Credenciais inválidas' })
+  }
+
+  if (!loginUser) {
+    throw createError({ statusCode: 401, message: 'Credenciais inválidas' })
+  }
+
+  const token = await generateJWTToken(loginUser)
+
+  const loginUserData = {
+    id: loginUser?.id,
+    name: loginUser?.name,
+    email: loginUser?.email
+  }
+
+  return {
+    token,
+    user: loginUserData
+  }
+})
