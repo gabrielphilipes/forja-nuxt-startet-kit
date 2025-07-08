29c23e11f4c8a59ffb3c662325a847d61a11ade2
Gabriel Philipe S <48845302+gabrielphilipes@users.noreply.github.com>
2025-07-08 14:00:44 -0300

    feat: add Forja logo

--- app/components/layout/header.vue
+++ app/components/layout/header.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+  const siteName = process.env.SITE_NAME || 'Forja'
+</script>
+
 <template>
   <header class="flex items-center justify-between w-full h-16 px-4">
-    <NuxtLink to="/" class="w-full h-full flex items-center justify-between">
-      <h1 class="text-2xl font-bold text-gray-900 dark:text-white">Forja</h1>
+    <NuxtLink to="/" class="flex items-center justify-between w-full h-full">
+      <img src="assets/images/logo.svg" :alt="siteName" class="size-16 dark:invert" />
     </NuxtLink>
 
     <div v-if="$slots.actions">
